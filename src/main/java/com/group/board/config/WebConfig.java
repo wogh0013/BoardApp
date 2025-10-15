b56e8477518703f4493fcb30166032c2504cfee7
wogh0013 <wogh0013@naver.com>
2025-10-16 00:29:35 +0900

    CORS 허용 (프론트->백엔드 접근 가능)

--- src/main/java/com/group/board/config/WebConfig.java
+++ src/main/java/com/group/board/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.group.board.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -18,4 +19,13 @@ public class WebConfig implements WebMvcConfigurer {
                         , "/css/**", "/js/**", "/images/**" //정적 리소스
                 );
     }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET","POST","PUT","DELETE")
+                .allowCredentials(true);
+    }
+
 }
