4398b5d373169e8778fe5666d4dbd9693391026a
wogh0013 <wogh0013@naver.com>
2025-10-18 03:29:43 +0900

    로그인 인터셉터 수정

--- src/main/java/com/group/board/config/WebConfig.java
+++ src/main/java/com/group/board/config/WebConfig.java
@@ -1,7 +1,6 @@
 package com.group.board.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,18 +13,10 @@ public class WebConfig implements WebMvcConfigurer {
                 .addPathPatterns("/**")     //전체 경로 검사
                 .excludePathPatterns(       //로그인 예외 경로
                         "/"
-                        , "/login"
-                        , "/join"
+                        , "/login/**"
+                        , "/join/**"
                         , "/css/**", "/js/**", "/images/**" //정적 리소스
                 );
-    }
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET","POST","PUT","DELETE")
-                .allowCredentials(true);
     }
 
 }
